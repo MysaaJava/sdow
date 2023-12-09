56025e307a14b0a926d854b4f2ab7dd98b33bf0a
MysaaJava <samyavrillon@netcourrier.com>
2023-12-09 19:18:43 +0100

    Fixed nginx moving dockerfile

--- nginx_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-WWW_DIR=/usr/share/nginx/html
-for file in $(find "$WWW_DIR" -type f)
-do
-    sed "s@\\\$#SDOW_API_URL#\\\$@$SDOW_API_URL@" -i "$file"
-done
-
-[ -z "$@" ] && nginx -g 'daemon off;' || $@
